79ecdf9ff83294fa8bbe9ab55485476c1e112dfe
shahnmah <shahn.mah@gmail.com>
2022-04-17 01:51:56 +0600

    banner added

--- src/components/Home/Banner/Banner.js
+++ src/components/Home/Banner/Banner.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { Carousel } from 'react-bootstrap';
+import './banner.css'
+import bgImg from '../../../images/bg-1.png'
 
 const Banner = () => {
     return (
-        <div>
-             <h1>this is banner</h1>
-             
+        <div className='banner container'>
+            <div className="row align-items-center">
+                <div className="col-lg-6">
+                    <h1>Your Travel Assistent in Bangladesh</h1>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facilis voluptate necessitatibus perspiciatis culpa, porro optio magni voluptatibus sit cumque minima.</p>
+                    <button className='btn btn-warning fw-bold py-2 px-4'>Contact Me</button>
+                </div>
+                <div className="col-lg-6 d-flex justify-content-center">
+                    <img className='img-fluid' src={bgImg} alt="" />
+                </div>
+            </div>
         </div>
     );
 };
--- src/components/Home/Home/Home.js
+++ src/components/Home/Home/Home.js
@@ -5,7 +5,6 @@ import Services from '../Services/Services';
 const Home = () => {
     return (
         <div>
-            <h1>this is home</h1>
             <Banner></Banner>
             <Services></Services>
         </div>
