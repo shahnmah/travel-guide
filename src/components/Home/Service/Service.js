bfff7cb3717fa43218d655a343abb99bec3af589
shahnmah <shahn.mah@gmail.com>
2022-04-17 02:39:51 +0600

    services added

---
+++ src/components/Home/Service/Service.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Button, Card } from 'react-bootstrap';
+
+const Service = ({ service }) => {
+    const { title, img, price, description } = service
+    return (
+            <div className="col-lg-4">
+                <Card style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src={img} />
+                    <Card.Body>
+                        <Card.Title>{title}</Card.Title>
+                        <p>Price: {price}</p>
+                        <Card.Text>
+                            {description}
+                        </Card.Text>
+                        <Button variant="primary">Book Now</Button>
+                    </Card.Body>
+                </Card>
+            </div>
+    );
+};
+
+export default Service;
--- src/components/Home/Services/Services.js
+++ src/components/Home/Services/Services.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Service from '../Service/Service';
 
 const Services = () => {
+    const [services, setServices] = useState([])
+    useEffect(() => {
+        fetch('services.json')
+            .then(res => res.json())
+            .then(data => setServices(data))
+    }, [])
     return (
-        <div>
-            <h1>this is services</h1>
+        <div className="container">
+            <div className='row'>
+                {
+                    services.map(service => <Service key={service.id} service={service}></Service>)
+                }
+            </div>
         </div>
     );
 };
